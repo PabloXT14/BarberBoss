60bbd553bdbae32716bd538ed10ec0201ed5f060
Pablo Alan <pabloxt14@gmail.com>
2026-08-28 00:13:06 -0300

    refactor: edit billings table amount column format

--- src/BarberBoss.Infrastructure/DataAccess/BarberBossDbContext.cs
+++ src/BarberBoss.Infrastructure/DataAccess/BarberBossDbContext.cs
@@ -11,4 +11,16 @@ internal class BarberBossDbContext : DbContext
 
     // OBS: O nome da propriedade DbSet deve ser o mesmo nome da tabela no banco de dados (não precisa ser case-sensitive), caso contrário, será necessário configurar o mapeamento no OnModelCreating.
     public DbSet<Billing> Billings { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configurações adicionais de mapeamento podem ser feitas aqui, se necessário.
+        modelBuilder.Entity<Billing>(entity =>
+        {
+            entity.Property(billing => billing.Amount)
+                .HasPrecision(10, 2);
+        });
+    }
 }
